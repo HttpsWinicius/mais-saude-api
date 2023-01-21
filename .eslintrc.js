e29d225aba49157d8c4e2f7cdb5f8fb05e84b27f
Thiago Puntar <thiagopuntar@gmail.com>
2023-01-21 15:51:31 -0300

    project config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,12 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: 'airbnb-base',
-  overrides: [
-  ],
+  extends: ["airbnb-base", "prettier"],
+  overrides: [],
+  plugins: ["prettier"],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  rules: {
-  },
+  rules: {},
 };
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const { PORT } = process.env;
+
+const app = express();
+app.listen(() => console.log(`Running on port ${PORT}`));
