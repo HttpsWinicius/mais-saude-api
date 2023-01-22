61f4ba479ded910dfc47a253a4a9deb1910ef04e
Thiago Puntar <thiagopuntar@gmail.com>
2023-01-22 05:29:35 -0300

    update vaccination

--- src/modules/vaccination/vaccination.controller.js
+++ src/modules/vaccination/vaccination.controller.js
@@ -5,30 +5,30 @@ import axios from "axios";
 export const updateVaccination = async (req, res) => {
   try {
     await dbClient("tbl_vaccination")
-      .where(
-        "id_person",
-        "=",
-        req.user,
-        "id_vaccine",
-        "=",
-        req.body.idVaccine
-      )
+      .where("id_person", "=", req.user, "id_vaccine", "=", req.body.idVaccine)
       .update({
         date: req.body.date,
-        batch: req.body.batch
+        batch: req.body.batch,
       });
 
     const resultVaccine = await getPeriodicity(req.body.idVaccine);
-    const schedule_date = dayjs(req.body.date).add(resultVaccine.periodicity, 'day').format('YYYY-MM-DD');
+    const schedule_date = dayjs(req.body.date)
+      .add(resultVaccine.periodicity, "day")
+      .format("YYYY-MM-DD");
     const resultPerson = await getNamePerson(req.user);
 
     await dbClient("tbl_vaccination").insert({
       id_person: req.user,
       id_vaccine: req.body.idVaccine,
-      schedule_date
-    })
+      schedule_date,
+    });
 
-    await sendSms(resultPerson.name, resultVaccine.name, schedule_date, resultPerson.phone);
+    await sendSms(
+      resultPerson.name,
+      resultVaccine.name,
+      schedule_date,
+      resultPerson.phone
+    );
 
     res.status(200).json("Success");
   } catch (e) {
@@ -37,28 +37,31 @@ export const updateVaccination = async (req, res) => {
   }
 };
 
-
 const sendSms = async (namePerson, nameVaccine, scheduleDate, telephone) => {
+  scheduleDate = dayjs(scheduleDate).format("DD/MM/YYYY");
 
-  scheduleDate = dayjs(scheduleDate).format('DD/MM/YYYY');
-
-  const textMessage = "Atenção!! " + namePerson + ", sua proxima vacina de " + nameVaccine
-    + " foi agendada para " + scheduleDate
-    + " mais não se preocupe, te lembraremos no dia.";
+  const textMessage =
+    "Atenção!! " +
+    namePerson +
+    ", sua proxima vacina de " +
+    nameVaccine +
+    " foi agendada para " +
+    scheduleDate +
+    " mais não se preocupe, te lembraremos no dia.";
 
   var data = JSON.stringify({
-    "numberPhone": telephone,
-    "message": textMessage,
-    "flashSms": true
+    numberPhone: telephone,
+    message: textMessage,
+    flashSms: true,
   });
 
   var config = {
-    method: 'post',
-    url: 'https://api.nvoip.com.br/v2/sms?napikey=MGlJQWpsbjVSSkQ5NWxNSEwzMWtUNWNPOElHT05naGU=',
+    method: "post",
+    url: "https://api.nvoip.com.br/v2/sms?napikey=MGlJQWpsbjVSSkQ5NWxNSEwzMWtUNWNPOElHT05naGU=",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    data: data
+    data: data,
   };
 
   axios(config)
@@ -68,7 +71,7 @@ const sendSms = async (namePerson, nameVaccine, scheduleDate, telephone) => {
     .catch(function (error) {
       console.log(error);
     });
-}
+};
 
 const getPeriodicity = async (idVaccine) => {
   const [result] = await dbClient
@@ -90,6 +93,25 @@ const getNamePerson = async (idPerson) => {
   return result;
 };
 
+export const getStatus = (vaccination) => {
+  if (!vaccination.date && !vaccination.schedule_date) return "Não informado";
+
+  if (vaccination.date) {
+    return "Em dia";
+  }
+
+  if (dayjs(vaccination.schedule_date).isBefore(dayjs())) {
+    return "Em atraso";
+  }
+
+  return "Em dia";
+};
+
+export const formatDate = (dateValue) => {
+  if (!dateValue) return dateValue;
+  return dayjs(dateValue).format("DD/MM/YYYY");
+};
+
 export const getVaccination = async (req, res) => {
   const data = await dbClient
     .select(
@@ -108,5 +130,12 @@ export const getVaccination = async (req, res) => {
     )
     .where("vaccination.id_person", req.user);
 
-  res.json(data);
+  const vaccinations = data.map((vaccination) => ({
+    ...vaccination,
+    date: formatDate(vaccination.date),
+    schedule_date: formatDate(vaccination.schedule_date),
+    status: getStatus(vaccination),
+  }));
+
+  res.json(vaccinations);
 };
