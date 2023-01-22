4d85bdca8706587017157b801405291083fcd4ca
Winicius Souza <winiciussouzadev@gmail.com>
2023-01-21 23:27:57 -0300

    Bug: Coluna created_at

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,7 +1,7 @@
 import { dbClient } from "../config/database.js";
 
 export const authenticateMiddleware = async (req, res, next) => {
-  const [user] = await dbClient("tbl_person").orderBy("createdAt");
+  const [user] = await dbClient("tbl_person").orderBy("created_at");
   console.log(">>> auth", user);
   req.user = 6;
   next();
